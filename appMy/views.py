c0203b668bf2a4ac2138a0ab6641adf7cf44de13
Özgün Taner Kara <117898000+OzgunTKara@users.noreply.github.com>
2023-04-06 17:47:50 +0300

    Add files via upload

---
+++ appMy/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from appUser.models import Profile
+
+# Create your views here.
+
+def index(request):
+    context = {}
+    return render(request,'index.html', context)
+
+def indexBrowse(request, slug):
+    profil = Profile.objects.filter(user = request.user).get(slug=slug)
+    
+    context = {
+        "profil": profil,
+    }
+    return render(request,'browse-index.html', context)
+
+def seriesTabs(request,):
+    
+    context = {
+        "series": seriesTabs
+    }
+
+    return render(request, 'series.html', context)
+
+def filmsTabs(request,):
+    context = {
+
+    }
+    return render(request, 'films.html', context)
